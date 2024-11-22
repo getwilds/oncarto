49f7658d05e28003f7ffe48a7d7f2147ce02f69c
vsriram24 <vsriram@fredhutch.org>
2024-11-22 10:20:47 -0800

    Update data filtering to dplyr

--- R/filter-input-data.R
+++ R/filter-input-data.R
@@ -3,10 +3,10 @@
 #'
 filter_input_data <- function(input_data, cancer_type, race, sex, age, stage, year) {
   input_data |>
-    filter(cancer_type == cancer_type) |>
-    filter(race == race) |>
-    filter(sex == sex) |>
-    filter(age == age) |>
-    filter(stage == stage) |>
-    filter(year == year)
+    dplyr::filter(cancer_type == cancer_type) |>
+    dplyr::filter(race == race) |>
+    dplyr::filter(sex == sex) |>
+    dplyr::filter(age == age) |>
+    dplyr::filter(stage == stage) |>
+    dplyr::filter(year == year)
 }
