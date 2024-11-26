cc113f502c1863cbd805a5ee5657ae981f59dc01
vsriram24 <vsriram@fredhutch.org>
2024-11-25 16:41:08 -0800

    Functionalize palette

--- R/server-county-incidence.R
+++ R/server-county-incidence.R
@@ -9,17 +9,19 @@
 server_county_incidence <- function(id, func_to_apply, data_table_name,
                                     state_abbr) {
   shiny::moduleServer(id, function(input, output, session) {
+    # Warning message if map can't be shown
+    warning_message <- "<h4>No cancer incidence data are available for this specific combination of inputs. Please try a different combination of inputs.</h4>"
+
+    # Get the shape file that includes county boundaries for the given US state
+    county_boundaries <- get_county_boundaries(state_abbr)
+
+    # Get input cancer incidence data based on the user-provided get_data() fn
+    # and table name
     func <- get(func_to_apply)
     input_data <- func(data_table_name)
 
-    county_boundaries <- get_county_boundaries(state_abbr)
-
     output$choropleth <- leaflet::renderLeaflet({
-      # county_level_incidence <- filter_input_data(input_data, input$cancer_type,
-      #                                             input$race, input$sex,
-      #                                             input$age, input$stage,
-      #                                             input$year)
-
+      # Filter the incidence data by the input parameters
       county_level_incidence <- input_data |>
         dplyr::filter(cancer_type == input$cancer_type) |>
         dplyr::filter(race == input$race) |>
@@ -28,12 +30,13 @@ server_county_incidence <- function(id, func_to_apply, data_table_name,
         dplyr::filter(stage == input$stage) |>
         dplyr::filter(year == input$year)
 
-      # write_map_message(county_level_incidence)
+      #write_map_message(county_level_incidence)
 
-      # If data do not exist for this combination of inputs, print a warning message
+      # If data do not exist for this combination of inputs, print a warning and
+      # do not return a map
       if(all(is.na(county_level_incidence$Age_Adjusted_Incidence_Rate))){
         output$map_message <- renderUI({
-          HTML("<h4>No cancer incidence data are available for this specific combination of inputs. Please try a different combination of inputs.</h4>")
+          HTML(paste(warning_message))
         })
 
         return(NULL)
@@ -41,6 +44,7 @@ server_county_incidence <- function(id, func_to_apply, data_table_name,
 
       # Otherwise, we can print our map
       else {
+        # Set warning message to NULL
         output$map_message <- renderUI({
           NULL
         })
@@ -49,11 +53,13 @@ server_county_incidence <- function(id, func_to_apply, data_table_name,
         county_level_incidence_with_shape <- county_boundaries |>
           left_join(county_level_incidence, by = "County")
 
-        # Generate color palette based on the selected cancer data (FH yellow)
-        pal <- colorNumeric(
-          c("#F4F4F4", "#FFB500"),
-          domain = county_level_incidence_with_shape$Age_Adjusted_Incidence_Rate,
-          na.color = "transparent"
+        # Generate color palette based on the selected cancer data
+        pal <- make_palette(
+          county_level_incidence_with_shape,
+          "Age_Adjusted_Incidence_Rate",
+          "#F4F4F4",
+          "#FFB500",
+          "transparent"
         )
 
         # Create an interactive choropleth map using {leaflet}
@@ -73,7 +79,7 @@ server_county_incidence <- function(id, func_to_apply, data_table_name,
               fillOpacity = 0.7,
               bringToFront = TRUE
             ),
-            label = ~paste(NAMELSAD, ": ", Age_Adjusted_Incidence_Rate),
+            label = ~paste(County, ": ", Age_Adjusted_Incidence_Rate),
             labelOptions = labelOptions(
               style = list("font-weight" = "normal", padding = "3px 8px"),
               textsize = "15px",
@@ -89,7 +95,6 @@ server_county_incidence <- function(id, func_to_apply, data_table_name,
           )
       }
 
-      #NULL
     })
 
   })
--- R/write-map-message.R
+++ R/write-map-message.R
@@ -2,13 +2,17 @@
 #' @importFrom shiny renderUI
 #'
 write_map_message <- function(filtered_data) {
-  print(head(filtered_data))
   out <- NULL
+
   if(all(is.na(filtered_data$Age_Adjusted_Incidence_Rate))) {
     out <- HTML("<h4>No cancer incidence data are available for this specific combination of inputs. Please try a different combination of inputs.</h4>")
   } else {
     print("Safe!")
   }
 
-  return(shiny::renderUI({out}))
+  return(
+    shiny::renderUI({
+      out
+    })
+  )
 }
